07baf0f6a9d63dfed92c924109cc06a494cefd9c
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-04 19:50:11 -0300

    Metodo customizado 'saleSum()' criado no repositories, com consulta SQL e paginaçãao

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -9,10 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleSumProjection;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 	
-	
+	//Consulta com JPQL
 	@Query(value = "SELECT obj "
 			+ "FROM Sale obj "
 			+ "JOIN FETCH obj.seller "
@@ -22,4 +23,11 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 			countQuery = "SELECT COUNT(obj) FROM Sale obj JOIN obj.seller")
 	Page<Sale> reportSale(LocalDate dateMin, LocalDate dateMax, String seller, Pageable pageable);
 	
+	
+	//Consulta com SQL	
+	@Query(nativeQuery = true, value = "SELECT TB_SELLER.NAME, SUM(AMOUNT) "
+			+ "FROM TB_SALES "
+			+ "INNER JOIN TB_SELLER ON TB_SALES.SELLER_ID = TB_SELLER.ID "
+			+ "GROUP BY TB_SELLER.NAME")
+	Page<SaleSumProjection> saleSum(Pageable pageable);
 }
